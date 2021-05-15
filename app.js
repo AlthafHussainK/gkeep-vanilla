b6289d57411213b291c23b8b01ad75654bcd91b8
AlthafHussainK <althafakm@gmail.com>
2021-05-15 14:01:17 +0530

    store notes in app

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 class App {
   constructor() {
+    this.notes = []
+
     this.$form = document.querySelector('#form')
     this.$noteTitle = document.querySelector('#note-title')
+    this.$noteText = document.querySelector('#note-text')
     this.$formButtons = document.querySelector('#form-buttons')
 
     this.addEventListeners()
@@ -10,6 +13,17 @@ class App {
   addEventListeners() {
     document.body.addEventListener('click', event => {
       this.handleFormClick(event)
+    })
+
+    this.$form.addEventListener('submit', event => {
+      event.preventDefault()
+      const title = this.$noteTitle.value
+      const text = this.$noteText.value
+      const hasNote = title || text
+      if (hasNote) {
+        //add note
+        this.addNote({ title, text })
+      } 
     })
   }
 
@@ -33,6 +47,17 @@ class App {
     this.$form.classList.remove('form-open')
     this.$noteTitle.style.display = 'none'
     this.$formButtons.style.display = 'none'
+  }
+
+  addNote(note) {
+    const newNote = {
+      title: note.title,
+      text: note.text,
+      color: 'white',
+      id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 1
+    }
+    this.notes = [...this.notes, newNote]
+    console.log(this.notes)
   }
 
 }
