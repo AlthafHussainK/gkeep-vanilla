4573b048000db97b5caa4eb4ddb87ecb1ff7ed47
AlthafHussainK <althafakm@gmail.com>
2021-05-15 13:12:41 +0530

    expand note input on click

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
 class App {
   constructor() {
-    console.log('app works')
+    this.$form = document.querySelector('#form')
+    this.$noteTitle = document.querySelector('#note-title')
+    this.$formButtons = document.querySelector('#form-buttons')
+
+    this.addEventListeners()
   }
+
+  addEventListeners() {
+    document.body.addEventListener('click', event => {
+      this.handleFormClick(event)
+    })
+  }
+
+  handleFormClick(event) {
+    const isFormClicked = this.$form.contains(event.target)
+
+    if (isFormClicked) {
+      this.openForm()
+    } else {
+      // close form
+    }
+  }
+
+  openForm() {
+    this.$form.classList.add('form-open')
+    this.$noteTitle.style.display = 'block'
+    this.$formButtons.style.display = 'block'
+  }
+
 }
 
 new App()
