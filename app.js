4753f2978e0512bdedf52883172b843ff5839735
AlthafHussainK <althafakm@gmail.com>
2021-05-16 11:30:55 +0530

    deleting notes

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ class App {
       this.handleFormClick(event)
       this.selectNote(event)
       this.openModal(event)
+      this.deleteNote(event)
     })
 
     document.body.addEventListener('mouseover', event => {
@@ -103,6 +104,8 @@ class App {
   }
 
   openModal(event) {
+    if(event.target.matches('.toolbar-delete')) return
+
     if (event.target.closest('.note')) {
       this.$modal.classList.toggle('open-modal')
       this.$modalTitle.value = this.title
@@ -111,7 +114,6 @@ class App {
   }
 
   closeModal(event) {
-    
     this.editNote()
     this.$modal.classList.toggle('open-modal')
   }
@@ -169,6 +171,14 @@ class App {
     this.id = $selectedNote.dataset.id
   }
 
+  deleteNote(event) {
+    event.stopPropagation()
+    if (!event.target.matches('.toolbar-delete')) return
+    const id = event.target.dataset.id
+    this.notes = this.notes.filter(note => note.id !== Number(id))
+    this.displayNotes()
+  }
+
   displayNotes() {
     const hasNotes = this.notes.length > 0
     this.$placeholder.style.display = hasNotes ? 'none' : 'flex'
@@ -180,7 +190,7 @@ class App {
         <div class="toolbar-container">
           <div class="toolbar">
             <img class="toolbar-color" data-id=${note.id} src="" alt="">⚙️
-            <img class="toolbar-delete" src="" alt="">⚙️
+            <img class="toolbar-delete" data-id=${note.id} src="" alt="">⚙️
           </div>
         </div>
       </div>
