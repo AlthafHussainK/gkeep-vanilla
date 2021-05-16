4542d9d45264ecccd11ef7bada169e819d7e9b60
AlthafHussainK <althafakm@gmail.com>
2021-05-16 07:02:06 +0530

    add note on click

--- app.js
+++ app.js
@@ -32,9 +32,15 @@ class App {
   handleFormClick(event) {
     const isFormClicked = this.$form.contains(event.target)
 
+    const title = this.$noteTitle.value
+    const text = this.$noteText.value
+    const hasNote = title || text
+
     if (isFormClicked) {
       this.openForm()
-    } else {
+    } else if (hasNote) {
+      this.addNote({ title, text })
+    }else {
       this.closeForm()
     }
   }
@@ -53,10 +59,10 @@ class App {
     this.$noteText.value = ''
   }
 
-  addNote(note) {
+  addNote({ title, text }) {
     const newNote = {
-      title: note.title,
-      text: note.text,
+      title,
+      text,
       color: 'white',
       id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 1
     }
